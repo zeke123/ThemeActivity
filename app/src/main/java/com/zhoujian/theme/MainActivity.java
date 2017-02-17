23bed98c7b5ca68e184c08b3ad335fd4e70fa1d0
zhoujian <1064791247@qq.com>
2017-02-17 17:14:42 +0800

    Initial commit

---
+++ app/src/main/java/com/zhoujian/theme/MainActivity.java
@@ -0,0 +1,31 @@
+package com.zhoujian.theme;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button  bt_button = (Button)findViewById(R.id.bt_button);
+
+
+        bt_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
